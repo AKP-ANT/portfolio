e1624cfae5835a91f3dddec553f5f33741135f90
AKP-ANT <pinsonanthony@gmail.com>
2024-03-09 13:22:11 -0500

    view-project-button-fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
             <h2 className='text-3xl sm:text-5xl font-bold text-blue-light'>Aspiring Robotics Software Engineer</h2>
             <p className='text-gray py-4 max-w-[700px]'>This page is dedicated to the projets I have been passionate to create.</p>
             <div>
-                <button className='text-gray group border-2 px-6 py-3 my-2 flex item-center hover:bg-blue-dark hover:border-blue-dark hover:text-base'>
-                    <Link to='portfolio' smooth={true} duration={500}>
-                        View Projects
-                    </Link>
-                </button>
+                <Link to='portfolio' smooth={true} duration={500}>
+                    <button className='text-gray group border-2 px-6 py-3 my-2 flex item-center hover:bg-blue-dark hover:border-blue-dark hover:text-base'>
+                        <p>View Projects</p>
+                    </button>
+                </Link>
             </div>
         </div>
     </div>
